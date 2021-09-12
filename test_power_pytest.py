66777794be74b2fa45282f8e2f79551dfedc6051
takakiyuto <yuto@takakiyutonoMacBook-Pro.local>
2021-09-12 14:44:37 +0900

    2021-09-12

---
+++ test_power_pytest.py
@@ -0,0 +1,25 @@
+import pytest
+
+from power import power, times, divide
+
+def test_power():
+    base = 2
+    exp = 3
+    assert power(base, exp) == 8
+    with pytest.raises(TypeError):
+        power('3', '2')
+
+def test_times():
+    num1 = 2
+    num2 = 3
+    assert times(num1, num2) == 6
+
+def test_devide():
+    num1 = 4
+    num2 = 2
+    assert divide(num1, num2) == 2
+
+def test_devide_by_zero():
+    num1 = 4
+    num2 = 0
+    assert divide(num1, num2) is None
